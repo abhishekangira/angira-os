1c9c05333750456db5c694e28c31a179eb683c5f
Abhishek Angira <abhishek@angira.net>
2023-06-10 22:55:21 +0530

    added screen - booting

---
+++ src/desktop/screens/Booting.tsx
@@ -0,0 +1,26 @@
+import { Motion } from "@motionone/solid";
+import { Component } from "solid-js";
+
+const Booting: Component<{
+  setState: (state: "askFullscreen" | "booting" | "input" | "done") => void;
+}> = (props) => {
+  return (
+    <Motion.div style={{ display: "grid", "place-items": "center" }}>
+      <Motion.h1
+        animate={{ opacity: [0, 1] }}
+        transition={{
+          duration: 1,
+          easing: "ease-in-out",
+          repeat: 4,
+          // @ts-ignore
+          direction: "alternate",
+        }}
+        onMotionComplete={() => props.setState("input")}
+      >
+        Booting
+      </Motion.h1>
+    </Motion.div>
+  );
+};
+
+export default Booting;
